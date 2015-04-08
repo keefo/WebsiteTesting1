79f51161d12ee54c43dda669d89e0fd06dca9b05
Xu Lian <lianxu@me.com>
2015-04-08 16:32:02 -0700

    update php page

--- update.php
+++ update.php
@@ -1,3 +1,17 @@
 <?php
 
 
+echo exec('git pull origin master;');
+
+?>
+
+<br/><br/>
+
+<script type="text/javascript">   
+function Redirect() 
+{  
+    window.location="./"; 
+} 
+document.write("You will be redirected bank in 5 seconds"); 
+setTimeout('Redirect()', 5000);   
+</script>
